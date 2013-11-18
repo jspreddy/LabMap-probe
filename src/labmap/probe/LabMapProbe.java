c7dba8c25366b8ecc780dd218a5fdeab2a41c695
pratyushgupta55 <pratyushgupta55@gmail.com>
2013-11-17 22:24:19 -0500

    encoded json
    tags are: username, system

--- src/labmap/probe/LabMapProbe.java
+++ src/labmap/probe/LabMapProbe.java
@@ -7,15 +7,15 @@
 package labmap.probe;
 
 import com.google.gson.*;
+import java.io.BufferedReader;
+import java.io.DataOutputStream;
 import java.io.IOException;
-import java.io.PrintWriter;
+import java.io.InputStreamReader;
 import java.net.HttpURLConnection;
 import java.net.InetAddress;
-import java.net.MalformedURLException;
 import java.net.URL;
 import java.net.URLEncoder;
 import java.net.UnknownHostException;
-import java.util.Scanner;
 
 /**
  *
@@ -27,7 +27,7 @@ public class LabMapProbe {
     /**
      * @param args the command line arguments
      */
-    public static void main(String[] args) throws UnknownHostException {
+    public static void main(String[] args) throws UnknownHostException, IOException {
         UserData user = new UserData(getUsername(), getHostname());
         Gson gson = new Gson();
         String json = gson.toJson(user);
@@ -40,28 +40,26 @@ public class LabMapProbe {
     public static String getHostname() throws UnknownHostException {
         return InetAddress.getLocalHost().getHostName();
     }
-    public static void sendData(String json) {
-        HttpURLConnection conn;
-        try{
-            String param = URLEncoder.encode("json", "UTF-8") + "=" + URLEncoder.encode(json, "UTF-8");
+    public static void sendData(String json) throws IOException {
+       
+        URL siteUrl = new URL("http://localhost/LabMap/test.php");
+        HttpURLConnection conn = (HttpURLConnection) siteUrl.openConnection();
+        conn.setRequestMethod("POST");
+        conn.setDoOutput(true);
+        conn.setDoInput(true);
 
-            URL myurl = new URL("http://localhost/LabMap/test.php");
-            conn=(HttpURLConnection)myurl.openConnection();
-            //set the output to true, indicating you are outputting(uploading) POST data
-            //conn.setDoOutput(true);
-            //once you set the output to true, you don't really need to set the request method to post, but I'm doing it anyway
-            conn.setRequestMethod("POST");
-
-            conn.setFixedLengthStreamingMode(param.getBytes().length);
-            conn.setRequestProperty("Content-Type", "application/x-www-form-urlencoded");
-            //send the POST out
-            PrintWriter out = new PrintWriter(conn.getOutputStream());
-            out.write(param);
-            out.close();
-
-            }catch(MalformedURLException ex){
-            }catch(IOException ex){
+        DataOutputStream out = new DataOutputStream(conn.getOutputStream());
+        String content = "json=" + URLEncoder.encode(json, "UTF-8");
+        System.out.println(content);
+        out.writeBytes(content);
+        out.flush();
+        out.close();
+        BufferedReader in = new BufferedReader(new InputStreamReader(conn.getInputStream()));
+        String line = "";
+        while((line=in.readLine())!=null) {
+                System.out.println(line);
         }
+        in.close();
     }
     
 }
